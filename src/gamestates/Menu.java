eb56c05f3f861dea04c89f907f8fba8976582ee0
jaron <jaronwilson2025@gmail.com>
2022-12-03 10:37:46 -0500

    MENU https://www.youtube.com/watch?v=zGrLCHROa4s

--- src/gamestates/Menu.java
+++ src/gamestates/Menu.java
@@ -1,28 +1,53 @@
 package gamestates;
 
 import main.Game;
+import ui.MenuButton;
+import utilz.LoadSave;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 public class Menu extends State implements Statemethods{
+
+    private MenuButton[] buttons = new MenuButton[3];
+    private BufferedImage backgroundImage;
+    private int menuX, menuY, menuWidth, menuHeight;
 
 
     public Menu(Game game) {
         super(game);
+        loadButtons();
+        loadBackground();
+    }
+
+    private void loadBackground() {
+        backgroundImage = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND);
+        menuWidth = (int) (backgroundImage.getWidth() * Game.SCALE);
+        menuHeight = (int) (backgroundImage.getHeight() * Game.SCALE);
+        menuX  = Game.GAME_WIDTH /2 - menuWidth /2;
+        menuY  = (int) (45 * Game.SCALE);
+    }
+
+    private void loadButtons() {
+        buttons[0] = new MenuButton(Game.GAME_WIDTH /2, (int) (150 * Game.SCALE), 0, Gamestate.PLAYING);
+        buttons[1] = new MenuButton(Game.GAME_WIDTH /2, (int) (220 * Game.SCALE), 1, Gamestate.OPTIONS);
+        buttons[2] = new MenuButton(Game.GAME_WIDTH /2, (int) (290 * Game.SCALE), 2, Gamestate.QUIT);
     }
 
     @Override
     public void update() {
-
+        for (MenuButton menuButton : buttons)
+            menuButton.update();
     }
 
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.BLACK);
-        g.drawString("MENU", Game.GAME_WIDTH /2, 200);
 
+        g.drawImage(backgroundImage, menuX,menuY,menuWidth,menuHeight,null);
+        for (MenuButton menuButton : buttons)
+            menuButton.draw(g);
     }
 
     @Override
@@ -32,16 +57,41 @@ public class Menu extends State implements Statemethods{
 
     @Override
     public void mousePressed(MouseEvent e) {
+        for (MenuButton menuButton : buttons) {
+            if (isIn(e,menuButton)) {
+                menuButton.setMousePressed(true);
+                break;
+            }
+        }
 
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        for (MenuButton menuButton : buttons) {
+            if (isIn(e, menuButton)) {
+                if (menuButton.isMousePressed())
+                    menuButton.applyGamestate();
+                break;
+            }
+        }
+        resetButtons();
+    }
 
+    private void resetButtons() {
+        for (MenuButton menuButton : buttons)
+            menuButton.resetBools();
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
+        for (MenuButton menuButton : buttons)
+            menuButton.setMouseOver(false);
+        for (MenuButton menuButton : buttons)
+            if (isIn(e,menuButton)) {
+                menuButton.setMouseOver(true);
+                break;
+            }
 
     }
 
--- src/gamestates/State.java
+++ src/gamestates/State.java
@@ -1,6 +1,9 @@
 package gamestates;
 
 import main.Game;
+import ui.MenuButton;
+
+import java.awt.event.MouseEvent;
 
 public class State {
 
@@ -8,6 +11,9 @@ public class State {
 
     public State(Game game) {
         this.game = game;
+    }
+    public boolean isIn(MouseEvent e, MenuButton menuButton) {
+        return menuButton.getBounds().contains(e.getX(), e.getY());
     }
 
     public Game getGame() {
